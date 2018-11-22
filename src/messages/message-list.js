fe4d301edc094e99de5ec499e9254a535d8f87d8
Balaji Damarla <balaji.damarla@metricstream.com>
2018-11-22 21:41:36 +0530

    Final code of message-app project

---
+++ src/messages/message-list.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import MessageView from './message-view';
+
+class MessageList extends Component {
+    state = {
+        messages: [
+            {
+                from: 'John',
+                content: 'The event will start next week',
+                status: 'unread'
+            },
+            {
+                from: 'Martha',
+                content: 'I will be travelling soon',
+                status: 'read'
+            },
+            {
+                from: 'Jacob',
+                content: 'Talk later. Have a great day!',
+                status: 'read'
+            }
+        ]
+    }
+
+    render(){
+        const messageView = this.state.messages.map(function(message,index){
+            return (
+                <MessageView key={index} message={message} />
+            )
+        });
+
+        return (
+            <div>
+            <h1>List of Messages</h1>
+            {messageView}
+            </div>
+        )
+    }
+}
+
+export default MessageList;
